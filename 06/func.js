2da9dfa3e6e6215ebcaa1fc9c76ef85c9df9142c
skhuhise <skhuhise@naver.com>
2019-04-01 16:12:41 +0900

    0401 study

---
+++ 06/func.js
@@ -0,0 +1,16 @@
+f123();
+console.log('A aa');
+console.log('B bb');
+f123();
+console.log('Fk t');
+console.log('Cq a');
+console.log('Pzz e');
+f123();
+console.log('Umoa');
+f123();
+
+function f123() {
+    console.log(1);
+    console.log(2);
+    console.log(3);
+};
---
+++ 06/testServer2.js
@@ -0,0 +1,83 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+var app = http.createServer(function(request, response) {
+    var requestUrl = request.url;
+    var queryData = url.parse(requestUrl, true).query;
+    var pathName = url.parse(requestUrl, true).pathname;
+
+
+    if(pathName === '/') {
+        fs.readdir('../data', (err, fileList) => {
+            console.log(fileList);
+
+            var orderList = '<ol>';
+            var unorderList = '<ul>';
+            var i = 0;
+            while(i < fileList.length) {
+                orderList = orderList + `<li><a href="/?id=${fileList[i]}">${fileList[i]}</a></li>`;
+                unorderList += `<li><a href="/?id=${fileList[i]}">${fileList[i]}</a></li>`;
+                i++;
+            }
+
+            orderList = orderList + '</ol>';
+            unorderList += '</ul>';
+
+            if(queryData.id === undefined) {
+                var title = 'Welcome';
+                var description = 'Hello, good luck';
+
+                var template =`
+                <!doctype html>
+                <html>
+                <head>
+                    <title>WEB1 - ${title}</title>
+                    <meta charset="utf-8">
+                </head>
+                <body>
+                    <h1><a href="/">Testing</a></h1>
+                    <h2>${title}</h2>
+                    ${orderList}
+                    ${unorderList}
+                    <p>${title}${description}</p>
+                </body>
+                </html>
+                `;
+    
+                response.writeHead(200);
+                response.end(template);
+            }
+
+            else {
+                fs.readFile(`../data/${queryData.id}`, 'utf8', (err, data) => {
+                    var title = queryData.id;
+                    var description = data;
+                    var template =`
+                    <!doctype html>
+                    <html>
+                    <head>
+                        <title>WEB1 - ${title}</title>
+                        <meta charset="utf-8">
+                    </head>
+                    <body>
+                        <h1><a href="/">Testing</a></h1>
+                        <h2>${title}</h2>
+                        ${orderList}
+                        ${unorderList}
+                        <p>${title}${description}</p>
+                    </body>
+                    </html>
+                    `;
+
+                    response.writeHead(200);
+                    response.end(template);
+                });
+            }
+        });
+    } else {
+        response.writeHead(404);
+        response.end("Not found");
+    }
+});
+app.listen(8090);
