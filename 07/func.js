94f808c7ccd919efb49aaf32b9e868494748db32
skhuhise <skhuhise@naver.com>
2019-04-02 18:15:22 +0900

    0402 study

---
+++ 07/func.js
@@ -0,0 +1,17 @@
+console.log(Math.round(1.6));
+console.log(Math.round(1.4));
+console.log(Math.round(1.5));
+console.log(Math.round(1.51));
+console.log(Math.round(1.49));
+
+function sum(first, second) {
+    console.log(first + second);
+}
+
+sum(2, 4);
+
+function sum2(first, second) {
+    return first + second;
+}
+
+console.log(sum2(2, 4));
---
+++ 07/testServer.js
@@ -0,0 +1,80 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+function templateHTML(title, list, body) {
+    return `
+    <!doctype html>
+    <html>
+    <head>
+        <title>WEB1 - ${title}</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <h1><a href="/">Testing</a></h1>
+        <h2>${title}</h2>
+        ${list}
+        ${body}
+    </body>
+    </html>
+    `;
+}
+
+function templateList(fileList) {
+    var orderList = '<ol>';
+    var unorderList = '<ul>';
+    var i = 0;
+
+    while(i < fileList.length) {
+        orderList = orderList + `<li><a href="/?id=${fileList[i]}">${fileList[i]}</a></li>`;
+        unorderList = unorderList + `<li><a href="/?id=${fileList[i]}">${fileList[i]}</a></li>`;
+        i++;
+    }
+
+    orderList += '</ol>';
+    unorderList += '</ul>';
+
+    var list = orderList + unorderList;
+
+    return list;
+}
+
+var app = http.createServer(function(request, response) {
+    var requestUrl = request.url;
+    var queryData = url.parse(requestUrl, true).query;
+    var pathName = url.parse(requestUrl, true).pathname;
+
+
+    if(pathName === '/') {
+        if(queryData.id === undefined) {
+            fs.readdir('../data', (err, fileList) => {
+                var title = 'Welcome';
+                var description = 'Hello, good luck';
+                var list = templateList(fileList);
+                var template = templateHTML(title, list, `<p>${title}${description}</p>`)
+    
+                response.writeHead(200);
+                response.end(template);
+            });
+            
+        } else {
+
+            fs.readdir('../data', (err, fileList) => {
+                fs.readFile(`../data/${queryData.id}`, 'utf8', (err, data) => {
+                    var title = queryData.id;
+                    var description = data;
+                    var list = templateList(fileList);
+                    var template = templateHTML(title, list, `<p>${title}${description}</p>`)
+
+                    response.writeHead(200);
+                    response.end(template);
+                });
+            });
+            
+        }
+    } else {
+        response.writeHead(404);
+        response.end("Not found");
+    }
+});
+app.listen(8090);
